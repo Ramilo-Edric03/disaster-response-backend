3664ff276594a7157304261bc11a794309a7685f
Ramilo-Edric03 <edric.ramilo03@gmail.com>
2025-03-01 14:24:37 +0800

    updated server.js

--- server.js
+++ server.js
@@ -5,39 +5,48 @@ const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "*", // Allow all origins (change this for security)
         methods: ["GET", "POST"]
     }
 });
 
 app.use(cors());
-let requests = [];
+
+let requests = []; // Store active requests
 
 io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
+    console.log("A user connected:", socket.id);
 
+    // Handle new help requests
     socket.on("sendRequest", (data) => {
+        console.log("New request received:", data);
         requests.push(data);
         io.emit("updateRequests", requests);
-        console.log("New help request:", data);
     });
 
-    socket.on("getRequests", () => {
-        socket.emit("updateRequests", requests);
-    });
-
-    socket.on("acceptRequest", ({ lat, lng, volunteerLat, volunteerLng }) => {
-        io.emit("matchRequest", { lat, lng, volunteerLat, volunteerLng });
-        console.log("Request accepted, route will be drawn.");
+    // Handle volunteer accepting a request
+    socket.on("acceptRequest", (data) => {
+        console.log("Request accepted:", data);
+        
+        // Remove accepted request from list
+        requests = requests.filter(req => req.lat !== data.lat || req.lng !== data.lng);
+        
+        // Notify all volunteers about updated requests
+        io.emit("updateRequests", requests);
+        
+        // Notify requester that a volunteer is coming
+        io.emit("requestAccepted", data);
     });
 
     socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
+        console.log("A user disconnected:", socket.id);
     });
 });
 
+// Default API route
 app.get("/", (req, res) => {
     res.send("Disaster Response WebSocket Server is Running!");
 });
